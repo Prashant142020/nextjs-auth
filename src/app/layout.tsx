82dcead5c5ec271becfe2397225fee8e5d618f11
Prashant142020 <prashant142020@gmail.com>
2023-10-23 20:45:06 +0530

    fix issue of globals.css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import Navbar from '@/components/Navbar';
 import Provider from '@/components/Provider';
  
 import { Toaster } from '@/components/ui/toaster';
-import '@/globals.css';
+import './globals.css'
  
  
 import type { Metadata } from 'next';
